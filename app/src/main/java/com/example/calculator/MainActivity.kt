70b7e4ca0b9dc1bd500b95e09779251dab2728d4
tujokuus <72389463+tujokuus@users.noreply.github.com>
2025-02-07 15:47:10 +0200

    updated buttons and result

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMultiply : Button
+    lateinit var btnDivision : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTv : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +29,40 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtraction)
+        btnMultiply = findViewById(R.id.btn_multiply)
+        btnDivision = findViewById(R.id.btn_division)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultTv = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMultiply.setOnClickListener(this)
+        btnDivision.setOnClickListener(this)
+
     }
+
+    override fun onClick(v: View?) {
+        var a  = etA.text.toString().toDouble()
+        var b  = etB.text.toString().toDouble()
+        var result = 0.0
+        when(v?.id){
+            R.id.btn_add ->{
+                result = a+b
+            }
+            R.id.btn_subtraction ->{
+                result = a-b
+            }
+            R.id.btn_multiply->{
+                result = a*b
+            }
+            R.id.btn_division->{
+                result = a/b
+            }
+        }
+        resultTv.text = "Result is $result"
+    }
+
 }
